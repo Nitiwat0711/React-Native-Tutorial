8508e44372987bf8072433f52e90605c1f7e1d2c
Nitiwat0711 <nitiwat.a@ku.th>
2022-01-13 01:14:23 +0700

    add lottie

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -8,6 +8,7 @@
  * @flow strict-local
  */
 import 'react-native-reanimated';
+import LottieView from 'lottie-react-native';
 import { MotiView } from 'moti';
 import React, {useState} from 'react';
 import {
@@ -16,47 +17,11 @@ import {
 	View,
 } from 'react-native';
 
-const LoadingIndicator = ({size})=> {
-		return (
-		<MotiView
-			from={{
-				width: size,
-				height: size,
-				borderRadius: size / 2,
-				borderWidth: 0,
-				shadowOpacity: 0.5,
-			}}
-			animate={{
-				width: size + 20,
-				height: size + 20,
-				borderRadius: (size + 20) / 2,
-				borderWidth: size / 10,
-				shadowOpacity: 1,
-			}}
-			transition={{
-				type: 'timing',
-				duration: 1000,
-				loop: true,
-			}}
-			style={{
-				width: size,
-				height: size,
-				borderRadius: size / 2,
-				borderWidth: size / 10,
-				borderColor: '#fff',
-				shadowColor: '#fff',
-				shadowOffset: { width: 0, height: 0 },
-				shadowOpacity: 1,
-				shadowRadius: 10,
-			}}
-		/>
-	);
-};
 
 const App = () => {
 	return (
 		<View style={styles.body}>
-			<LoadingIndicator size={100} />
+			<LottieView style={styles.loading} source={require('./assets/83685-hubit.json')} autoPlay loop/>
 		</View>
 	);
 };
@@ -64,12 +29,15 @@ const App = () => {
 const styles = StyleSheet.create({
 	body: {
 		flex: 1,
-		backgroundColor: '#010100',
+		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
 	text: {
-		color: '#ffffff',
+		color: '#000',
+	},
+	loading: {
+		margin: 10,
 	},
 });
 
